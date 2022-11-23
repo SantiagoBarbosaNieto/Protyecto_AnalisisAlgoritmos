88d931b970909f1f82e7662f4be9cd0f6d8d0e80
Santiago Barbosa <santibar15@hotmail.com>
2022-11-23 03:10:14 -0500

    Sirve con puzzles no tan dificiles

--- src/test.py
+++ src/test.py
@@ -7,6 +7,12 @@ import numpy as np
 import pathlib
 
 selectedNum : str = '0'
+mat : np.ndarray
+
+matPosibilidades: dict = {}
+
+posibilidades = set()
+originales = set()
 
 def selectNum(but):
     if but != None:
@@ -18,7 +24,14 @@ def handle(but):
         but.setText(selectedNum)
 
 def cargarMat(path):
+    global matPosibilidades
+    global posibilidades
+    global originales
+    matPosibilidades = {}
+    posibilidades = set()
+    originales = set()
     prePath = pathlib.Path(__file__).parent.resolve()
+    global mat
     try:
         with open(str(prePath)+"\\"+path) as f:
             lines = f.readlines()
@@ -35,13 +48,20 @@ def cargarMat(path):
         return mat
     mat = np.zeros((x,y))
     mat.fill(-np.inf)
-    for line in lines[1:-1]:
-        vals = line.split()
-        y = int(vals[0])
-        x = int(vals[2])
-        num = int(vals[4])
-        mat[x-1][y-1] = num
-    return mat
+    for i in range(1,len(lines)):
+        try:
+            line = lines[i]
+            vals = line.split()
+            y = int(vals[0])
+            x = int(vals[2])
+            num = int(vals[4])
+            mat[x-1][y-1] = num #En ejemplo.txt los indicies deben ser desde 1, no desde 0
+            posibilidades.add(num)
+            originales.add((x-1, y-1))
+        except:
+            continue
+    print("terminar")
+
 
 def resetGame(but):
     global board
@@ -51,46 +71,162 @@ def resetGame(but):
 def loadGame(but):
     global board
     global path
+    global mat
     print(path)
-    mat = cargarMat(path)
+    cargarMat(path)
     matSize = mat.shape
     recSize = (700,700)
     pos = (50,50)
     board = ui.Board(matSize, pos, recSize,mat, False, handle)
-    path = ''
+    
+def getVecinos(xPos, yPos):
+    vecinos = set()
+    for i in range(-1,2):
+        for j in range(-1,2):
+            if not (i == 0 and j == 0) and not(abs(i) == abs(j)):
+                global mat
+                shape = mat.shape
+                if(xPos+i >= 0 and yPos+j >= 0 and xPos+i < shape[0] and yPos+j < shape[1]):
+                    vecinos.add((xPos+i,yPos+j))
+    return vecinos
+
+def generarMatBot():
+    global mat
+    global matPosibilidades
+    for i in range(mat.shape[0]):
+        for j in range(mat.shape[1]):
+            if (i,j) in originales:
+                matPosibilidades[(i,j)] = [int(mat[i][j])]
+            else:
+                matPosibilidades[(i,j)] = list(posibilidades)
+
+def getSalidas(ij):  #Ambos parametros son tuplas
+    global matPosibilidades
+    vecinos = getVecinos(ij[0], ij[1])
+    salidas = set()
+    for v in vecinos:
+        vPosi = matPosibilidades.get(v)
+        ijPosi = matPosibilidades.get(ij)
+        if len(vPosi) > 1:
+            for p in ijPosi: 
+                if p in vPosi:
+                    salidas.add(v)
+    return salidas
+
+def getFijos(ij):
+    global matPosibilidades
+    vecinos = getVecinos(ij[0], ij[1])
+    fijos = set()
+    for v in vecinos:
+        vPosi = matPosibilidades.get(v)
+        if len(vPosi) == 1:
+            fijos.add(v)
+    return fijos
+
+def gameFinished():
+    finished = True
+    for key in matPosibilidades.keys():
+        if len(matPosibilidades.get(key)) != 1:
+            finished = False
+            break
+    return finished
+
+def updateMat():
+    for key in matPosibilidades.keys():
+        i,j = key
+        if len(matPosibilidades.get(key)) == 1:
+            mat[i][j] = matPosibilidades.get(key)[0]
+    global board
+    board.generateButtons(mat)
 
 def resolverGame(but):
-    print("resolver")
+    cargarMat(path)
+    generarMatBot()
+    global mat
+    global matPosibilidades
     
-def jugar(but):
-    mat = cargarMat(path)
-    for i in range(7):
-        for j in range(7):
-            if mat[i][j]== int:
-                aux=mat[i][j]
-                if mat[i][j+1] =='-' and mat[i][j-1]=='-' and mat[i+1][j]=='-' and mat[i-1][j]=='-':
-                    salidas=4
-                elif mat[i][j+1] and mat[i][j-1] and mat[i+1][j]=='-' or mat[i+1][j] and mat[i-1][j] and mat[i][j+1] =='-' or mat[i][j+1] and mat[i][j-1] and mat[i-1][j]=='-' or mat[i][j-1] and mat[i+1][j] and mat[i-1][j]=='-':
-                    salidas=3
-                elif mat[i][j+1] and mat[i][j-1]=='-'or mat[i+1][j] and mat[i-1][j]=='-' or mat[i-1][j] and mat[i][j+1]=='-' or mat[i-1][j] and mat[i][j-1]=='-' or mat[i+1][j] and mat[i][j+1]=='-':
-                    salidas=2
-                elif mat[i][j+1] =='-' or mat[i][j-1] or mat[i+1][j] or mat[i-1][j]:
-                    salidas=1 
-            if salidas <=2:
-                esFijo= True
-            else:
-                esFijo=False  
-            if esFijo==True:
-                if mat[i][j+1]=='-':
-                    mat[i][j+1]=aux
-                if mat[i][j-1]=='-':
-                    mat[i][j-1]=aux
-                if mat[i+1][j]=='-':
-                    mat[i+1][j]=aux
-                if mat[i-1][j]=='-':
-                    mat[i-1][j]=aux
+    iter = 0
+    while not gameFinished():
+        iter+=1
+        print("Iteration: ", iter)
+        for i in range(mat.shape[0]):
+            for j in range(mat.shape[1]):
+                vecinos = getVecinos(i,j)
+                salidas = getSalidas((i,j))
+                fijos = getFijos((i,j))
+                ijPosi = matPosibilidades.get((i,j))
+                        
+                #Aqui ya sabemso cuantas salidas tiene
                 
-            
+                if len(ijPosi) == 1: #si es fijo
+                    if (i,j) in originales: #Si es original
+                    
+                        camino = False
+                        for v in vecinos:
+                            vPosi = matPosibilidades.get(v)
+                            if len(vPosi) == 1 and vPosi[0] == ijPosi[0]:
+                                camino = True
+                                break
+
+                        if camino:   #Si ya tiene su camino (tiene 1 vecino fijo con su mismo #) Se verifica que las salidas al rededor ya no tengan el numero de esta casilla como posibilidad
+                            for s in salidas:
+                                setPS = set(matPosibilidades.get(s))
+                                if ijPosi[0] in setPS:
+                                    setPS.remove(ijPosi[0])
+                                matPosibilidades[s] = list(setPS)
+                        else: #Si no tiene su camino:
+                            if len(salidas) == 1: #Si tiene 1 salida:
+                                sal = list(salidas)[0]
+                                #esa salida se vuelve el mismo #
+                                matPosibilidades[sal] = ijPosi.copy()
+                    else: #No es original
+                        camino = False
+                        count = 0
+                        for v in vecinos:
+                            vPosi = matPosibilidades.get(v)
+                            if len(vPosi) == 1 and vPosi[0] == ijPosi[0]:
+                                count += 1
+                        if count >= 2:
+                            camino = True
+                        
+                        if camino: #Si ya tiene su camnio (tiene 2 vecinos fijos con su mismo #)
+                            for s in salidas:
+                                setPS = set(matPosibilidades.get(s))
+                                if ijPosi[0] in setPS:
+                                    setPS.remove(ijPosi[0])
+                                matPosibilidades[s] = list(setPS)
+                        else: #si no tiene camino
+                            if len(salidas) == 1: #Si tiene 1 salida:
+                                sal = list(salidas)[0]
+                                #esa salida se vuelve el mismo #
+                                matPosibilidades[sal] = ijPosi.copy()
+                else: #No es fijo
+                    if len(salidas) == 0: # si no tiene salidas
+                        if len(fijos) == 2: # si tiene 2 fijos alrededor
+                            matPosibilidades[(i,j)] = matPosibilidades.get(list(fijos)[0])#La casilla se vuelve el mismo numero que sus dos fijos
+                        elif len(fijos) == 3:
+                            if matPosibilidades.get(list(fijos)[0])[0] == matPosibilidades.get(list(fijos)[1])[0] or matPosibilidades.get(list(fijos)[0])[0] == matPosibilidades.get(list(fijos)[2])[0]: #Si el primer fijo es igual al segundo o tercer fijo
+                                matPosibilidades[(i,j)] = matPosibilidades.get(list(fijos)[0])
+                            else: # El segundo fijo y el tercer fijo son iguales, se puede usar cualquiera de esos dos
+                                matPosibilidades[(i,j)] = matPosibilidades.get(list(fijos)[1])
+
+                    elif len(salidas) == 1:
+                        setPos = set(ijPosi)
+                        setFijos = set()
+                        for f in fijos:
+                            setFijos.add(list(matPosibilidades.get(f))[0])
+                        setFinal = setPos.intersection(setFijos)
+                        matPosibilidades[(i,j)] = list(setFinal)
+                    elif len(salidas) == 2:
+                        setFinal = set()
+                        setSal1 = set(matPosibilidades.get(list(salidas)[0]))
+                        setSal2 = set(matPosibilidades.get(list(salidas)[1]))
+                        setFinal = setSal1.intersection(setSal2)
+                        matPosibilidades[(i,j)] = list(setFinal)
+        print(matPosibilidades)
+    #actualice mat con matPosibilidades que ya estan fijas
+    updateMat()
+
                 
 def main():
     pygame.init()
@@ -100,8 +236,9 @@ def main():
     SCREEN_HEIGHT = 800
 
     global path
-    path = 'Ejemplo.txt'
-    mat = cargarMat(path)
+    path = 'Ejemplo3.txt'
+    global mat
+    cargarMat(path)
     matSize = mat.shape
     recSize = (700,700)
     pos = (50,50)
